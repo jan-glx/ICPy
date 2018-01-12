cfad4bde0416bc947cc7c29ad222039de4f2f7f4
Jan Gleixner <jan.gleixner@gmail.com>
2018-01-12 15:57:21 +0100

    fix deployment instructions/info

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ICPy',
-      version='0.0.001',
+      version='0.0.002',
       description='Invariant Causal Prediction for python',
       url='https://github.com/jan-glx/ICPy',
       author='Jan Gleixner',
@@ -10,8 +10,18 @@ setup(name='ICPy',
       install_requires=[
           'numpy', 'scipy', 'scikit-learn',
       ],
+      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*,<4',
       packages=['icpy'],
-      test_suite='nose.collector',
-      tests_require=['nose'],
+      test_suite='tests',
+      classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+      ],
+      keywords = ['statistics', 'casual-inference'],
       include_package_data=True,
       zip_safe=False)
